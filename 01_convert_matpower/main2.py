de91f39a537987a65dfbb2aa656ea224e3c307eb
Jacob Kravits <kravitsjacob@gmail.com>
2021-09-09 11:04:11 -0600

    Update directories for matpower conversion

--- 01_convert_matpower/main2.py
+++ 01_convert_matpower/main2.py
@@ -1,9 +1,9 @@
 import pandapower.converter
 import os
 
-pathto_data = 'G:\My Drive\Documents (Stored)\data_sets\Convert Matpower V3_io'
-pathto_matpowercase = os.path.join(pathto_data, 'temp', 'case.mat')
-pathto_case = os.path.join(pathto_data, 'output', 'case.p')
+pathto_data = 'G:\My Drive\Documents (Stored)\data_sets\water-OPF-v0.1\synthetic_grid'
+pathto_matpowercase = os.path.join(pathto_data, 'case.mat')
+pathto_case = os.path.join(pathto_data, 'case.p')
 
 net = pandapower.converter.from_mpc(pathto_matpowercase)
 pandapower.to_pickle(net, pathto_case)
