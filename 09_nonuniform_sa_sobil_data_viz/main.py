ebed30d460902985f2dc35093afd7a412a40ca00
Jacob Kravits <kravitsjacob@gmail.com>
2021-09-09 23:42:57 -0600

    Updated plot labels

--- 09_nonuniform_sa_sobil_data_viz/main.py
+++ 09_nonuniform_sa_sobil_data_viz/main.py
@@ -1,4 +1,5 @@
 
+import numpy as np
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
@@ -23,7 +24,8 @@ def draw_heatmap(*args, **kwargs):
 def main():
     # Import Operational Scenarios
     df_operation = pd.read_csv(pathto_operation)
-    # Import Sobil Indices
+
+    # Import Sobol Indices
     lis_results = []
     for index, row in df_operation.iterrows():
         path = os.path.join(pathto_data, 'nonuniform_sa_sobol', row['Operational Scenario']+' sobol.csv')
@@ -31,18 +33,25 @@ def main():
         df_results['Scenario'] = row['Operational Scenario']
         lis_results.append(df_results)
     df_results = pd.concat(lis_results)
+
+    # Filter
+    df_results[df_results._get_numeric_data() < -1] = np.nan  # Invalid Index
+
     # Plotting
-    plt.figure(figsize=(15, 15))
-    g = sns.FacetGrid(df_results, col='Scenario', height=5, aspect=0.4, sharey=True)
-    cbar_ax = g.fig.add_axes([.91, .15, .03, .7])
-    g.map_dataframe(draw_heatmap, cmap='viridis', cbar_ax=cbar_ax, cbar_kws={'label': 'First Order Sobil Index Value'})
-    g.fig.subplots_adjust(top=.55, right=0.90)
+    df_results.columns = ['$C_{water,coal,once-through}$', '$C_{water,coal,recirculating}$',
+                          '$C_{water,coal,induced-recirculating}$', '$C_{water,natural-gas,induced-recirculating}$',
+                          '$C_{water,nuclear,recirculating}$', 'Scenario']
+
+    g = sns.FacetGrid(df_results, col='Scenario', height=6.5, aspect=0.35, sharey=True)
+    cbar_ax = g.fig.add_axes([.87, .15, .03, .7])
+    g.map_dataframe(draw_heatmap, cmap='viridis', cbar_ax=cbar_ax, cbar_kws={'label': 'First Order Sobol Index Value'})
+    g.fig.subplots_adjust(top=0.6, right=0.85)
     g.set_titles(rotation=90)
     axes = g.axes.flatten()
-    axes[0].set_title('Normal loading with traditional OPF (BAU policy)')
-    axes[1].set_title('Heatwave with traditional OPF (BAU policy)')
-    axes[2].set_title('Heatwave with aggressive withdrawal policy')
-    axes[3].set_title('Heatwave with aggressive consumption policy')
+    axes[0].set_title('Normal loading with \n traditional OPF (BAU policy)')
+    axes[1].set_title('Heatwave with traditional \n OPF (BAU policy)')
+    axes[2].set_title('Heatwave with aggressive  \n withdrawal policy')
+    axes[3].set_title('Heatwave with aggressive \n consumption policy')
     plt.savefig(os.path.join(pathto_figures, 'First Order Heatmap.pdf'))
     plt.show()
     return 0
