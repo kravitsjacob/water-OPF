56b61e66c05cb096f8e3b554a6b9e1b45bc21bbe
Jacob Kravits <kravitsjacob@gmail.com>
2021-09-15 16:53:45 -0600

    Facetgrid of generator output with withdrawal weight

--- 07_uniform_sa_data_viz/main.py
+++ 07_uniform_sa_data_viz/main.py
@@ -70,33 +70,48 @@ def get_gen_output_ratio_with_fuelcool(df, df_gen_info):
 
 
 def viz_effect_of_withdrawal_weight_gen_output(df):
-    # Selecting Data
-    df['Withdrawal Weight ($/Gallon)'] = pd.cut(df['Withdrawal Weight ($/Gallon)'], [0.0, 0.01, 0.1], labels=['0.0', '[0.01, 0.1]'], right=False)
+    # Formatting
+    df['MATPOWER Index'] = df['MATPOWER Index'].astype('str')
+
+    # Subsetting Data
     df = df[df['Consumption Weight ($/Gallon)'] == 0.0]
-    df = df[df['Uniform Water Factor'] == 0.5]
-    df = df[df['Fuel/Cooling Type'] == 'coal/OC']
+    withdrawal_criteria = [0.0, 0.1, 0.1]
+    uniform_water_criteria = [0.5, 0.5, 1.5]
+    case_a = (df['Withdrawal Weight ($/Gallon)'] == withdrawal_criteria[0]) & (df['Uniform Water Factor'] == uniform_water_criteria[0])
+    case_b = (df['Withdrawal Weight ($/Gallon)'] == withdrawal_criteria[1]) & (df['Uniform Water Factor'] == uniform_water_criteria[1])
+    case_c = (df['Withdrawal Weight ($/Gallon)'] == withdrawal_criteria[2]) & (df['Uniform Water Factor'] == uniform_water_criteria[2])
+    df = df[case_a | case_b | case_c]
+
+
     # Creating Plot
-    df_plot = df
-    fig, ax = plt.subplots(figsize=(8, 5))
-    df_plot['MATPOWER Index'] = df_plot['MATPOWER Index'].astype('str')
-    df_plot = df_plot.sort_values('Uniform Loading Factor', ascending=False)
-    sns.scatterplot(data=df_plot, x='Generator Output', y='MATPOWER Index', hue='Uniform Loading Factor', size='Uniform Loading Factor', sizes=(10, 200), style='Withdrawal Weight ($/Gallon)', ax=ax)
-    plt.legend(title='Uniform Loading Factor', bbox_to_anchor=(1.02, 1), loc='upper left', borderaxespad=0)
-    plt.tight_layout()
+    df['Withdrawal Weight ($/Gallon)'] = df['Withdrawal Weight ($/Gallon)'].astype('category')
+    df = df.sort_values(['Fuel/Cooling Type', 'Uniform Loading Factor'], ascending=False)
+    df['ID'] = '$w_{with}=$' + df['Withdrawal Weight ($/Gallon)'].astype(str) + ', $c_{water}=$' + df['Uniform Water Factor'].astype(str)
+    g = sns.FacetGrid(df, row='ID', col='Fuel/Cooling Type', aspect=1, sharex='col')
+    g.map_dataframe(sns.scatterplot, y='Generator Output', x='MATPOWER Index', hue='Uniform Loading Factor',
+                    size='Uniform Loading Factor', sizes=(20, 200), style='Withdrawal Weight ($/Gallon)', linewidth=0)
+    g.set_xticklabels(rotation=90)
+    g.add_legend()
+    g.fig.subplots_adjust(wspace=.05, hspace=.05)
+    for row in range(g.axes.shape[0]):
+        for col in range(g.axes.shape[1]):
+            if row == 0:
+                g.axes[row, col].set_title(df['Fuel/Cooling Type'].unique()[col])
+            else:
+                g.axes[row, col].set_title('')
+
+            if col == 0:
+                g.axes[row, col].set_ylabel('$c_{water}=$'+str(uniform_water_criteria[row]))
     plt.show()
-    return 0
+    return g
 
 
 def main():
     df = pd.read_csv(pathto_samples)
     df_gen_info = pd.read_csv(pathto_gen_info)
-    #viz_effect_of_withdrawal_weight(df).savefig(os.path.join(pathto_figures, 'Effect of Withdrawal Weight on Withdrawl.pdf'))
+    viz_effect_of_withdrawal_weight(df).savefig(os.path.join(pathto_figures, 'Effect of Withdrawal Weight on Withdrawl.pdf'))
     df_capacity_ratio = get_gen_output_ratio_with_fuelcool(df, df_gen_info)
-
-    viz_effect_of_withdrawal_weight_gen_output(df_capacity_ratio)
-
-    #df_fuelcool_output_ratio = get_fuelcool_output_ratio(df, df_gen_info)
-    #viz_6(df).fig.savefig(os.path.join(pathto_figures, 'Effect of Withdrawal Weight on Generator Output.pdf'))
+    viz_effect_of_withdrawal_weight_gen_output(df_capacity_ratio).fig.savefig(os.path.join(pathto_figures, 'Effect of Withdrawal Weight on Generator Output.pdf'))
     return 0
 
 
