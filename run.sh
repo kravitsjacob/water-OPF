2135e1bea8af1344ca2c6590e126ba4e2291f47c
Jacob Kravits <kravitsjacob@gmail.com>
2021-09-19 20:58:18 -0600

    Added python running

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+conda activate analysis
+
+# Input Files
+pathto_case='G:\\My Drive\\Documents (Stored)\\data_sets\Illinois Synthetic Grid\ACTIVSg200\\case_ACTIVSg200.m'
+
+# Temp Files
+#pathto_case_info='G:\My Drive\Documents (Stored)\data_sets\water-OPF-v1.0\temp\gen_info.csv', 
+#pathto_case_export='G:\My Drive\Documents (Stored)\data_sets\water-OPF-v1.0\temp\case.mat'
+
+# Convert Matlab format
+cd src
+matlab -nojvm -r 'to_MATPOWER; exit;'
+cd ..
+
+python main.py
