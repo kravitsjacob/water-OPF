9cf0f8a7b852abc1e4829a3084ba5280801426a0
Jacob Kravits <kravitsjacob@gmail.com>
2021-09-19 21:33:15 -0600

    Added generator_match

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ import src
 pathto_data = 'G:\My Drive\Documents (Stored)\data_sets\water-OPF-v1.0'
 pathto_matpowercase = os.path.join(pathto_data, 'temp', 'synthetic_grid', 'case.mat')
 pathto_case = os.path.join(pathto_data, 'temp', 'synthetic_grid', 'case.p')
+pathto_geninfo = os.path.join(pathto_data, 'temp', 'synthetic_grid', 'gen_info.csv')
+pathto_geninfo_match = os.path.join(pathto_data, 'temp', 'synthetic_grid', 'gen_info_match.csv')
 
 def main():
-    # Ordered Flow
+    # Ordered Analysis
     src.convert_matpower(pathto_matpowercase, pathto_case)
     print('Success: convert_matpower')
-
+    src.generator_match(pathto_geninfo, pathto_geninfo_match)
     return 0
 
 
