38094315195372dd2886a1f3a883c4a88d5ad98a
Jacob Kravits <kravitsjacob@gmail.com>
2021-09-20 21:31:41 -0600

    Update checkpoint to individual h5 files

--- main.py
+++ main.py
@@ -18,14 +18,17 @@ else:
 
 # Paths for checkpoints
 pathto_case = os.path.join(pathto_data, 'temp', 'synthetic_grid', 'case.p')
-pathto_geninfo = os.path.join(pathto_data, 'temp', 'synthetic_grid', 'gen_info.csv')
-pathto_geninfo_match = os.path.join(pathto_data, 'temp', 'synthetic_grid', 'gen_info_match.csv')
-pathto_h5 = os.path.join(pathto_data, 'temp', 'processing_data.h5')
+pathto_gen_info_match = os.path.join(pathto_data, 'temp', 'gen_info_match.h5')
+pathto_gen_info_match_water = os.path.join(pathto_data, 'temp', 'gen_info_match_water.h5')
+pathto_EIA = os.path.join(pathto_data, 'temp', 'EIA.h5')
+pathto_hnwc = os.path.join(pathto_data, 'temp', 'hnwc.h5')
+
 pathto_uniform_sa = os.path.join(pathto_data, 'temp', 'uniform_sa_results.csv')
 
 # Paths for external Inputs
-pathto_EIA = 'G:\My Drive\Documents (Stored)\data_sets\EIA_theremoelectric_water_use'
+pathto_EIA_raw = 'G:\My Drive\Documents (Stored)\data_sets\EIA_theremoelectric_water_use'
 pathto_matpowercase = os.path.join(pathto_data, 'temp', 'synthetic_grid', 'case.mat')
+pathto_geninfo = os.path.join(pathto_data, 'temp', 'synthetic_grid', 'gen_info.csv')
 
 # Paths for figures
 pathto_figures = os.path.join(pathto_data, 'figures')
@@ -42,33 +45,26 @@ def main():
         pandapower.to_pickle(net, pathto_case)  # Save checkpoint
 
     # Manual generator matching
-    s = pd.HDFStore(pathto_h5)
-    if 'df_gen_info_match' in s:
-        s.close()
-        df_gen_info_match = pd.read_hdf(pathto_h5, 'df_gen_info_match')  # Load checkpoint
+    if os.path.exists(pathto_gen_info_match):
+        df_gen_info_match = pd.read_hdf(pathto_gen_info_match, 'df_gen_info_match')  # Load checkpoint
     else:
         df_gen_info = pd.read_csv(pathto_geninfo)
         df_gen_info_match = src.generator_match(df_gen_info)
         print('Success: generator_match')
-        df_gen_info_match.to_hdf(pathto_h5, key='df_gen_info_match', mode='a')  # Save checkpoint
-
-    # Import EIA data
-    s = pd.HDFStore(pathto_h5)
-    if 'df_EIA' in s:
-        s.close()
-        df_EIA = pd.read_hdf(pathto_h5, 'df_EIA')  # Load checkpoint
-    else:
-        df_EIA = src.import_EIA(pathto_EIA)
-        print('Success: import_EIA')
-        df_EIA.to_hdf(pathto_h5, key='df_EIA', mode='a')  # Save checkpoint
+        df_gen_info_match.to_hdf(pathto_gen_info_match, key='df_gen_info_match', mode='w')  # Save checkpoint
 
     # Cooling system information
-    s = pd.HDFStore(pathto_h5)
-    if 'df_gen_info_match_water' in s:
-        s.close()
-        df_gen_info_match_water = pd.read_hdf(pathto_h5, 'df_gen_info_match_water')  # Load checkpoint
-        df_hnwc = pd.read_hdf(pathto_h5, 'df_hnwc')  # Load checkpoint
+    if os.path.exists(pathto_gen_info_match_water):
+        df_gen_info_match_water = pd.read_hdf(pathto_gen_info_match_water, 'df_gen_info_match_water')  # Load checkpoint
+        df_hnwc = pd.read_hdf(pathto_hnwc, 'df_hnwc')  # Load checkpoint
     else:
+        # Import EIA data
+        if os.path.exists(pathto_EIA):
+            df_EIA = pd.read_hdf(pathto_EIA, 'df_EIA')  # Load checkpoint
+        else:
+            df_EIA = src.import_EIA(pathto_EIA_raw)
+            print('Success: import_EIA')
+            df_EIA.to_hdf(pathto_EIA, key='df_EIA', mode='w')  # Save checkpoint
         df_gen_info_match_water, df_hnwc, fig_regionDistribututionPlotter, fig_regionBoxPlotter, fig_coalPlotter,\
         fig_hnwc_plotter = src.cooling_system_information(df_gen_info_match, df_EIA)
         print('Success: cooling_system_information')
@@ -76,8 +72,8 @@ def main():
         fig_regionBoxPlotter.savefig(os.path.join(pathto_figures, 'region water boxplots.pdf'))
         fig_coalPlotter.savefig(os.path.join(pathto_figures, 'coal scatter kmeans.pdf'))
         fig_hnwc_plotter.savefig(os.path.join(pathto_figures, 'historic nonuniform water coefficient histograms.pdf'))
-        df_gen_info_match_water.to_hdf(pathto_h5, key='df_gen_info_match_water', mode='a')  # Save checkpoint
-        df_hnwc.to_hdf(pathto_h5, key='df_hnwc', mode='a')  # Save checkpoint
+        df_gen_info_match_water.to_hdf(pathto_gen_info_match_water, key='df_gen_info_match_water', mode='w')  # Save checkpoint
+        df_hnwc.to_hdf(pathto_hnwc, key='df_hnwc', mode='w')  # Save checkpoint
 
 
     # Uniform SA
