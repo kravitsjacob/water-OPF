2eb37605bd1670e85bbc0c5cebf30763664cb02e
kravitsjacob <kravitsjacob@gmail.com>
2021-09-22 17:53:28 -0600

    Added computational power to non-uniform case

--- slurm_nonuniform_run.sh
+++ slurm_nonuniform_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --nodes=4
+#SBATCH --nodes=10
 #SBATCH --ntasks-per-node=24
 #SBATCH --cpus-per-task=1
 #SBATCH --output=run.out
@@ -8,7 +8,7 @@
 #SBATCH --partition=shas
 #SBATCH --qos=condo
 #SBATCH --account=ucb-summit-jrk
-#SBATCH --time=0-00:60:00
+#SBATCH --time=0-00:120:00
 #SBATCH --mail-user=kravitsjacob@gmail.com
 #SBATCH --mail-type=END
 
