0d1becf228c1dbd2ea447a09652dd6961c600b09
Jacob Kravits <kravitsjacob@gmail.com>
2021-09-22 15:25:32 -0600

    Create non-uniform sa slurm

---
+++ slurm_nonuniform_run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=24
+#SBATCH --cpus-per-task=1
+#SBATCH --output=run.out
+#SBATCH --job-name=gosox
+#SBATCH --partition=shas
+#SBATCH --qos=condo
+#SBATCH --account=ucb-summit-jrk
+#SBATCH --time=0-00:60:00
+#SBATCH --mail-user=kravitsjacob@gmail.com
+#SBATCH --mail-type=END
+
+module purge
+source /curc/sw/anaconda3/2019.07/bin/activate
+conda activate water-OPF
+python -u main.py '/scratch/summit/jakr3868/water-OPF-v1.0/' 24
