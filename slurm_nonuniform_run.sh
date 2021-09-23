cc8c613237ccb99559fdafb6f2aed43b88d86caf
kravitsjacob <kravitsjacob@gmail.com>
2021-09-22 21:34:13 -0600

    Adding more computing power

--- slurm_nonuniform_run.sh
+++ slurm_nonuniform_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --nodes=10
+#SBATCH --nodes=24
 #SBATCH --ntasks-per-node=24
 #SBATCH --cpus-per-task=1
 #SBATCH --output=run.out
@@ -8,7 +8,7 @@
 #SBATCH --partition=shas
 #SBATCH --qos=condo
 #SBATCH --account=ucb-summit-jrk
-#SBATCH --time=0-00:120:00
+#SBATCH --time=0-00:240:00
 #SBATCH --mail-user=kravitsjacob@gmail.com
 #SBATCH --mail-type=END
 
