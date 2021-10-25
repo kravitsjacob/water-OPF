9eb2891a6b7749a3840fbc8a13abe01db4dcd3c5
Jacob Kravits <kravitsjacob@gmail.com>
2021-10-25 16:08:15 -0600

    Change order of dtreeviz

--- run.sh
+++ run.sh
@@ -5,10 +5,10 @@ eval "$(conda shell.bash hook)"
 
 # Create conda environment
 conda env create -f water-OPF.yml
-pip install dtreeviz # Must be installed via pip
 
 # Load conda environment
 conda activate water-OPF
+pip install dtreeviz # Must be installed via pip
 
 # Convert Matlab format (output files supplied)
 #cd src
